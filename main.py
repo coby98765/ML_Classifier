2fd09ddbe65fcc121783a36503bf281ffff9799a
coby98765 <coby98765@gmail.com>
2025-07-09 17:20:37 +0300

    Predictor

--- main.py
+++ main.py
@@ -1,7 +1,45 @@
-# from predictor.predict import
+from predictor.predict import Predictor
 from trainer.train_model import Trainer
 
-data_csv = r"./data/phishing.csv"
+project = "phishing"
+data_csv = f"./data/{project}.csv"
 
-training = Trainer()
-training.train(data_csv,"phishing")
+# training = Trainer()
+# training.train(data_csv,project)
+
+predictor = Predictor(project)
+predictor.load_model()
+
+sample_input = {
+    "UsingIP": 1,
+    "LongURL": -1,
+    "ShortURL": 1,
+    "Symbol@": 1,
+    "Redirecting//": 1,
+    "PrefixSuffix-": -1,
+    "SubDomains": 0,
+    "HTTPS": -1,
+    "DomainRegLen": -1,
+    "Favicon": 1,
+    "NonStdPort": 1,
+    "HTTPSDomainURL": 1,
+    "RequestURL": -1,
+    "AnchorURL": 0,
+    "LinksInScriptTags": -1,
+    "ServerFormHandler": -1,
+    "InfoEmail": 1,
+    "AbnormalURL": 1,
+    "WebsiteForwarding": 0,
+    "StatusBarCust": 1,
+    "DisableRightClick": 1,
+    "UsingPopupWindow": 1,
+    "IframeRedirection": 1,
+    "AgeofDomain": -1,
+    "DNSRecording": 1,
+    "WebsiteTraffic": 0,
+    "PageRank": -1,
+    "GoogleIndex": 1,
+    "LinksPointingToPage": 0,
+    "StatsReport": 1
+}
+print(predictor.predict(sample_input))
---
+++ predictor/predict.py
@@ -0,0 +1,77 @@
+import json
+
+
+class Predictor:
+    def __init__(self,data_file_name):
+        self.file_adr = f"./model_data/{data_file_name}_trained_data.json"
+        self.data = None
+
+
+    def predict(self,entry):
+        cal = self._create_cal_dict(entry)
+        final_options = self._sum_key(cal)
+        normalized_keys = self._normalize_options(final_options)
+        normalized_vals = self._normalize_and_scale(normalized_keys)
+        print("Options: ",normalized_vals)
+        max_option = max(normalized_vals, key=normalized_vals.get)
+        return max_option
+
+    def load_model(self):
+        with open(self.file_adr, 'r') as f:
+            self.data = json.load(f)
+        # print(self.data)
+
+    #utils
+    #create a dict or the options based on model
+    def _create_cal_dict(self,entry):
+        if set(entry.keys()) != set(self.data["columns"][:-1]):
+            print("Data dos not match model.")
+            return None
+        cal = dict()
+        options = self.data["sum"].keys()
+        for option in options:
+            cal[option] = dict()
+            for col in entry.keys():
+                x = entry[col]
+                cal[option][col] = self.data["data"][option][col][str(x)]
+        return cal
+
+    #calculate options rate
+    @staticmethod
+    def _sum_key(options_data):
+        final_options = dict()
+        for key in options_data.keys():
+            key_sum = 1
+            for col in options_data[key].keys():
+                key_sum *= options_data[key][col]
+            final_options[key] = key_sum
+        return final_options
+    
+    @staticmethod
+    def _normalize_options(dict_in):
+        value_map = {
+            1: "yes",
+            -1: "no",
+            0: "none",
+            "1": "yes",
+            "-1": "no",
+            "0": "none"
+        }
+        return {
+            value_map.get(key, key) :value
+            for key, value in dict_in.items()
+        }
+
+    @staticmethod
+    def _normalize_and_scale(scores, epsilon=1e-9):
+        # Add small epsilon to avoid zeros
+        scores = {k: v + epsilon for k, v in scores.items()}
+
+        # Normalize to get probabilities that sum to 1
+        total = sum(scores.values())
+        normalized = {k: v / total for k, v in scores.items()}
+
+        # Optional: Rescale to percentages
+        scaled = {k: round(v * 100, 2) for k, v in normalized.items()}
+
+        return scaled
