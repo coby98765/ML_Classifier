898f8f48f2d1e7ff0a9cb7fad4ada47660407c92
coby98765 <coby98765@gmail.com>
2025-07-17 09:46:20 +0300

    added option to run backend from project main

--- main.py
+++ main.py
@@ -1,52 +1,6 @@
-# from back.predictor import Classifier
-# from back.trainer import Train
+from back.routes.api import app
 
-project = "phishing"
-data_csv = f"./data/{project}.csv"
-
-
-# training = Trainer()
-# initial_df = training.get_df(data_csv)
-# df_to_train,df_to_test = split_dataframe(initial_df)
-# training.train(df_to_train,project)
-# test = Test()
-# accuracy_rate = test.run_test(df_to_test)
-#
-# predictor = Classifier(project)
-# predictor.load_model()
-
-sample_input = {
-    "UsingIP": 1,
-    "LongURL": -1,
-    "ShortURL": 1,
-    "Symbol@": 1,
-    "Redirecting//": 1,
-    "PrefixSuffix-": -1,
-    "SubDomains": 0,
-    "HTTPS": -1,
-    "DomainRegLen": -1,
-    "Favicon": 1,
-    "NonStdPort": 1,
-    "HTTPSDomainURL": 1,
-    "RequestURL": -1,
-    "AnchorURL": 0,
-    "LinksInScriptTags": -1,
-    "ServerFormHandler": -1,
-    "InfoEmail": 1,
-    "AbnormalURL": 1,
-    "WebsiteForwarding": 0,
-    "StatusBarCust": 1,
-    "DisableRightClick": 1,
-    "UsingPopupWindow": 1,
-    "IframeRedirection": 1,
-    "AgeofDomain": -1,
-    "DNSRecording": 1,
-    "WebsiteTraffic": 0,
-    "PageRank": -1,
-    "GoogleIndex": 1,
-    "LinksPointingToPage": 0,
-    "StatsReport": 1
-}
-
-#print(predictor.predict(sample_input))
-
+# run back end
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8001)
